f5976f1fb7ceaa81aab0728e215c24c4a2834441
austindflatt <austindflatt@gmail.com>
2021-10-12 18:00:24 -0400

    created header and sneaker worth components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-
+import './style.css';
+import { Header } from './components/Header';
+import { Worth } from './components/Worth';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Header />
+      <div>
+        <Worth />
+      </div>
     </div>
   );
 }
---
+++ src/components/Worth.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Worth = () => {
+    return (
+        <div>
+            Your sneaker collection is worth 0
+        </div>
+    )
+}
