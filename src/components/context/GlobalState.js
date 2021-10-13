90d817f709b35eb8c0dcd313510ddf9554e6665c
austindflatt <austindflatt@gmail.com>
2021-10-12 20:28:00 -0400

    removed dummy sneakers in global state and changed to empty array

--- src/components/context/GlobalState.js
+++ src/components/context/GlobalState.js
@@ -3,12 +3,7 @@ import AppReducer from './AppReducer';
 
 // Initial state
 const initialState = {
-    sneakers: [
-    { id: 1, brand: 'Nike', model: 'Dunk Low', colorway: 'Michigan State', condition: 'new', size: 10, price: 300 },
-    { id: 2, brand: 'Nike', model: 'Dunk Low', colorway: 'Georgetown', condition: 'new', size: 10, price: 211 },
-    { id: 3, brand: 'Nike', model: 'Dunk Low', colorway: 'Black White', condition: 'new', size: 10, price: 279 },
-    { id: 4, brand: 'Nike', model: 'Dunk Low', colorway: 'Michigan', condition: 'new', size: 10, price: 187 },
-    ]
+    sneakers: []
 }
 
 // Create context
