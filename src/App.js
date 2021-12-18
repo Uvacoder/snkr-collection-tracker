468d91b6068a66954fa2cb447d2a23c818d7c98b
Uvacoder <88749643+Uvacoder@users.noreply.github.com>
2021-12-18 18:46:27 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './style.css';
 import { Header } from './components/Header';
-import { Worth } from './components/Worth';
+//import { Worth } from './components/Worth';
 import { SneakerList } from './components/SneakerList';
-import { AddSneaker } from './components/AddSneaker';
+//import { AddSneaker } from './components/AddSneaker';
 import { GlobalProvider } from './context/GlobalState';
 
 
